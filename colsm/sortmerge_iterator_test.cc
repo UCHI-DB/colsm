eede5c2437fa7a7f382a78b22fc539745e1a38ff
githubsigmod2021 <githubsigmod2021@gmail.com>
2021-04-19 00:32:59 -0500

    Working on Iterator

--- colsm/sortmerge_iterator_test.cc
+++ colsm/sortmerge_iterator_test.cc
@@ -11,14 +11,17 @@
 using namespace std;
 using namespace leveldb;
 
-class VectorIterator : public Iterator {
+class IntIterator : public Iterator {
 protected:
-    vector<pair<Slice, Slice>> &content_;
+    vector <int32_t> &content_;
     uint32_t pointer_ = -1;
     Status status_;
+
+    int32_t key_buffer_;
+    Slice slice_;
 public:
 
-    VectorIterator(vector<pair<Slice, Slice>> content) : content_(content) {}
+    IntIterator(vector <int32_t> content) : content_(content), slice_((const char *) &key_buffer_, 4) {}
 
     bool Valid() const override {
         return pointer_ >= 0 && pointer_ < content_.size();
@@ -26,10 +29,12 @@ public:
 
     void SeekToFirst() override {
         pointer_ = 0;
+        key_buffer_ = content_[0];
     }
 
     void SeekToLast() override {
         pointer_ = content_.size() - 1;
+        key_buffer_ = content_[pointer_];
     }
 
     void Seek(const Slice &target) override {
@@ -38,18 +43,22 @@ public:
 
     void Next() override {
         pointer_++;
+        if (pointer_ < content_.size())
+            key_buffer_ = content_[pointer_];
     }
 
     void Prev() override {
         pointer_--;
+        if (pointer_ >= 0)
+            key_buffer_ = content_[pointer_];
     }
 
     Slice key() const override {
-        return content_[pointer_].first;
+        return slice_;
     }
 
     Slice value() const override {
-        return content_[pointer_].second;
+        return slice_;
     }
 
     Status status() const override {
@@ -61,7 +70,8 @@ public:
 using namespace leveldb;
 
 TEST(TwoMergeIterator, Next) {
-
+    vector <int32_t> left_content{1, 3, 5, 7, 9, 10, 11};
+    vector <int32_t> right_content{2, 4, 6, 8, 10, 12};
 }
 
 // LevelDB test did not use gtest_main
