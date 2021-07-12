5c09e6097eca2114145406756823cc149752a5d9
harper <harperjiang@msn.com>
2021-07-11 23:19:00 -0500

    Developing solvers

--- colsm/cost/solver.cc
+++ colsm/cost/solver.cc
@@ -4,7 +4,37 @@
 
 #include "solver.h"
 
+#include <ilcplex/ilocplex.h>
+
 namespace colsm {
 
-void CPlexSolver::Solve(CostModel& model) {}
+using namespace std;
+
+void CPlexSolver::Solve(CostModel& cost_model) {
+  IloEnv env;
+  IloModel model(env);
+
+  IloNumVarArray var(env);
+  IloRangeArray con(env);
+
+  IloCplex cplex(model);
+  cplex.solve();
+
+  env.out() << "Solution status = " << cplex.getStatus() << endl;
+  env.out() << "Solution value  = " << cplex.getObjValue() << endl;
+
+  IloNumArray vals(env);
+  cplex.getValues(vals, var);
+  env.out() << "Values        = " << vals << endl;
+  cplex.getSlacks(vals, con);
+  env.out() << "Slacks        = " << vals << endl;
+
+  cplex.exportModel("mipex1.lp");
+
+  env.end();
+  return;
+
+}  // END main
+
+}
 }  // namespace colsm
